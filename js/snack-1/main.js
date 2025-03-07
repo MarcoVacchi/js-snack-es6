ab39daca65d41f584b5aa04c3540cbb3ca327a86
MarcoVacchi <mvacchi816@gmail.com>
2025-03-07 14:26:44 +0100

    feat: create array and objects

--- js/snack-1/main.js
+++ js/snack-1/main.js
@@ -3,4 +3,41 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.*/
 
+//array che contiene le mie bici;
+
+const bicycle = [
+
+    //oggetto bici con nome e peso;
+
+    {
+        name : "bianchi",
+        weidth : "20"
+    },
+
+    
+    {
+        name : "verdi",
+        weidth : "23"
+    },
+    
+    {
+        name : "rossi",
+        weidth : "10"
+    },
+    
+    {
+        name : "gialli",
+        weidth : "3"
+    },
+
+    {
+        name : "viola",
+        weidth : "5"
+    },
+
+
+]
+
+//console.log(bicycle);
+//console.log(bicycle[0].name, bicycle[1].name, bicycle[2].name, bicycle[3].name, bicycle[4].name);
 
